c1347d7881191a3b8a34acbec597b1f93fa5142c
MadMofin <Arthurjaziel1@live.com>
2023-04-24 16:13:19 -0600

    Add submit button and sort operation history

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -17,19 +17,6 @@ export const Calculator = () => {
   });
   const [history, setHistory] = React.useState([]);
 
-  React.useEffect(() => {
-    if (!(isNaN(parseFloat(data.a)) || isNaN(parseFloat(data.b)))) {
-      const res = operations.operation(
-        parseFloat(data.a),
-        parseFloat(data.b),
-        data.operation
-      );
-
-      setData({ ...data, result: res });
-    }
-    //eslint-disable-next-line
-  }, [data.a, data.b, data.operation]);
-
   /**
    * Adds a new data element to the history and limits the number of elements to a maximum of 10.
    *
@@ -40,10 +27,9 @@ export const Calculator = () => {
    * @returns {void}
    */
 
-  const addDataHistory = (newData, res, history, setHistory) => {
-    const newHistory = [...history, newData];
-    if (newHistory.length >= 10) newHistory.shift();
-    newHistory.push({ ...newData, result: res });
+  const addDataHistory = (newData) => {
+    const newHistory = [newData, ...history];
+    if (newHistory.length >= 10) newHistory.pop();
 
     setHistory(newHistory);
   };
@@ -56,26 +42,27 @@ export const Calculator = () => {
         result: "Enter a valid number",
       });
 
-    const newData = { ...data, [v.target.name]: v.target.value };
-    const res = operations.operation(
-      parseFloat(newData.a),
-      parseFloat(newData.b),
-      newData.operation
-    );
-    setData({ ...newData, result: res });
-    addDataHistory(newData, res, history, setHistory);
+    setData({ ...data, [v.target.name]: v.target.value });
   };
 
-  const handleSelect = (v) => {
-    const newData = { ...data, [v.target.name]: v.target.value };
-    const res = operations.operation(
-      parseFloat(newData.a),
-      parseFloat(newData.b),
-      newData.operation
-    );
+  const handleSubmit = () => {
+    if (!(isNaN(parseFloat(data.a)) || isNaN(parseFloat(data.b)))) {
+      const res = operations.operation(
+        parseFloat(data.a),
+        parseFloat(data.b),
+        data.operation
+      );
 
-    setData({ ...newData, result: res });
+      setData({
+        ...data,
+        result: res,
+      });
+      addDataHistory({ ...data, result: res });
+    }
   };
+
+  const handleSelect = (v) =>
+    setData({ ...data, [v.target.name]: v.target.value });
 
   React.useEffect(() => {
     localStorage.setItem("calculatorHistory", JSON.stringify(history));
@@ -100,13 +87,6 @@ export const Calculator = () => {
         data-testid="a"
         value={data.a}
       />
-      <input
-        style={styles.input}
-        onChange={handleChange}
-        name="b"
-        data-testid="b"
-        value={data.b}
-      />
       <select
         style={styles.select}
         onChange={handleSelect}
@@ -119,6 +99,27 @@ export const Calculator = () => {
         <option value="divide">Divide</option>
         <option value="multiply">Multiply</option>
       </select>
+      <input
+        style={styles.input}
+        onChange={handleChange}
+        name="b"
+        data-testid="b"
+        value={data.b}
+      />
+      <button
+        data-testid="submit"
+        style={{
+          height: 48,
+          width: 150,
+          color: "white",
+          backgroundColor: "hotpink",
+          borderRadius: 5,
+          border: 0,
+        }}
+        onClick={handleSubmit}
+      >
+        Calcular
+      </button>
       <div style={styles.result} data-testid="result" data-value={data.result}>
         {data.result !== null && "Result: " + data.result}
       </div>
