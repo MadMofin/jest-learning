79ec9c10d9cfdb0b13e4208a3eb0f7b48778b846
Jose Miguel <miguel.hdz.rayon@gmail.com>
2023-04-26 22:21:15 -0600

    cambios

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,9 +3,50 @@ import * as operations from "../utils/mathOperations";
 import { OPERATIONS_SYMBOLS } from "../constants/calculatorConstants";
 
 const styles = {
-  input: { margin: 10, height: 25, fontSize: 20, padding: 10 },
-  select: { margin: 10, height: 50, fontSize: 20, padding: 10 },
-  result: { margin: 10, height: 25, fontSize: 40, padding: 10 },
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    minHeight: "100vh",
+    backgroundColor: "#F6F6F6",
+    fontFamily: "sans-serif",
+  },
+  title: {
+    margin: 0,
+    fontSize: 36,
+    fontWeight: "bold",
+    color: "#333333",
+    marginBottom: 20,
+  },
+  input: {
+    margin: 10,
+    height: 40,
+    fontSize: 20,
+    padding: 10,
+    border: "1px solid #C4C4C4",
+    borderRadius: 5,
+    outline: "none",
+  },
+  select: {
+    margin: 10,
+    height: 50,
+    fontSize: 20,
+    padding: 10,
+    border: "1px solid #C4C4C4",
+    borderRadius: 5,
+    outline: "none",
+  },
+  result: {
+    margin: 10,
+    height: 35,
+    fontSize: 30,
+    padding: 10,
+    border: "1px solid #C4C4C4",
+    borderRadius: 5,
+    outline: "none",
+    backgroundColor: "#FFFFFF",
+  },
 };
 
 export const Calculator = () => {
@@ -19,13 +60,16 @@ export const Calculator = () => {
   let auxHistory = [];
 
   React.useEffect(() => {
-    if (!(isNaN(parseFloat(data.a)) || isNaN(parseFloat(data.b)))) {
+    if (
+      !(isNaN(parseFloat(data.a)) || isNaN(parseFloat(data.b))) &&
+      data.a !== "" &&
+      data.b !== ""
+    ) {
       const res = operations.operation(
         parseFloat(data.a),
         parseFloat(data.b),
         data.operation
       );
-
       setData({ ...data, result: res });
     }
     //eslint-disable-next-line
@@ -36,30 +80,32 @@ export const Calculator = () => {
       return setData({
         ...data,
         [v.target.name]: v.target.value,
-        result: "Enter a valid number",
+        result: null,
+        error: "Enter a valid number",
       });
 
-    const newData = { ...data, [v.target.name]: v.target.value };
-    const res = operations.operation(
-      parseFloat(newData.a),
-      parseFloat(newData.b),
-      newData.operation
-    );
-    setData({ ...newData, result: res });
-    const getHistory = operations.addDataHistory(newData, res, auxHistory);
-    setHistory(getHistory);
+    setData({ ...data, [v.target.name]: v.target.value });
   };
 
-  const handleSelect = (v) => {
-    const newData = { ...data, [v.target.name]: v.target.value };
-    const res = operations.operation(
-      parseFloat(newData.a),
-      parseFloat(newData.b),
-      newData.operation
-    );
+  const handleSubmit = () => {
+    if (!(isNaN(parseFloat(data.a)) || isNaN(parseFloat(data.b)))) {
+      const res = operations.operation(
+        parseFloat(data.a),
+        parseFloat(data.b),
+        data.operation
+      );
 
-    setData({ ...newData, result: res });
+      setData({
+        ...data,
+        result: res,
+      });
+      const getHistory = operations.addDataHistory(newData, res, auxHistory);
+      setHistory(getHistory);
+    }
   };
+
+  const handleSelect = (v) =>
+    setData({ ...data, [v.target.name]: v.target.value });
 
   React.useEffect(() => {
     localStorage.setItem("calculatorHistory", JSON.stringify(history));
@@ -75,35 +121,57 @@ export const Calculator = () => {
   }, []);
 
   return (
-    <div data-testid="calculator">
-      <h1>Simple calculator</h1>
-      <input
-        style={styles.input}
-        onChange={handleChange}
-        name="a"
-        data-testid="a"
-        value={data.a}
-      />
-      <input
-        style={styles.input}
-        onChange={handleChange}
-        name="b"
-        data-testid="b"
-        value={data.b}
-      />
-      <select
-        style={styles.select}
-        onChange={handleSelect}
-        name="operation"
-        data-testid="operator"
-        value={data.operation}
+    <div data-testid="calculator" style={styles.container}>
+      <h1 style={styles.title} data-testid="title">
+        Simple calculator :DDD
+      </h1>
+      <div>
+        <input
+          style={styles.input}
+          onChange={handleChange}
+          name="a"
+          data-testid="a"
+          value={data.a}
+        />
+        <select
+          style={styles.select}
+          onChange={handleSelect}
+          name="operation"
+          data-testid="operator"
+          value={data.operation}
+        >
+          <option value="sum">+</option>
+          <option value="substract">-</option>
+          <option value="divide">/</option>
+          <option value="multiply">*</option>
+        </select>
+        <input
+          style={styles.input}
+          onChange={handleChange}
+          name="b"
+          data-testid="b"
+          value={data.b}
+        />
+        <button
+          data-testid="submit"
+          style={{
+            height: 48,
+            width: 150,
+            color: "white",
+            backgroundColor: "hotpink",
+            borderRadius: 5,
+            border: 0,
+          }}
+          onClick={handleSubmit}
+        >
+          Calcular
+        </button>
+      </div>
+      <div
+        style={styles.result}
+        data-testid="result"
+        data-value={`Result: ${data.error ? data.error : data.result}`}
       >
-        <option value="sum">Sum</option>
-        <option value="substract">Substract</option>
-        <option value="divide">Divide</option>
-        <option value="multiply">Multiply</option>
-      </select>
-      <div style={styles.result} data-testid="result" data-value={data.result}>
         {data.result !== null && "Result: " + data.result}
       </div>
       <h3 style={{ paddingTop: 12 }}>History of last operations c:</h3>
